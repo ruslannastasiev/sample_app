2ad179bbace8c51e8d840eaefaad5c74a896dd87
«ruslannastasiev» <ruslan0607@gmail.com>
2013-03-10 14:02:18 +0200

    Done with sign up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,12 @@ module ApplicationHelper
 			base_title					
 		else
 			"#{base_title} | #{@title}"
-		end
-		
+		end		
 	end
+
+
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+  
 end
